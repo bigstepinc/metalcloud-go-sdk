5a11a6d67b0845e55f1add1fe1c0edc37200b200
costina.batica@bigstep.com <costina.batica@bigstep.com>
2020-11-05 11:00:18 +0200

    add volume template os bootstrap function name to VolumeTemplate

--- volume_template.go
+++ volume_template.go
@@ -11,18 +11,19 @@ type OperatingSystem struct {
 
 //VolumeTemplate describes an OS template
 type VolumeTemplate struct {
-	VolumeTemplateID                   int             `json:"volume_template_id,omitempty"`
-	VolumeTemplateLabel                string          `json:"volume_template_label,omitempty"`
-	VolumeTemplateSizeMBytes           int             `json:"volume_template_size_mbytes,omitempty"`
-	VolumeTemplateDisplayName          string          `json:"volume_template_display_name,omitempty"`
-	VolumeTemplateDescription          string          `json:"volume_template_description,omitempty"`
-	VolumeTemplateLocalDiskSupported   bool            `json:"volume_template_local_supported,omitempty"`
-	VolumeTemplateBootMethodsSupported string          `json:"volume_template_boot_methods_supported,omitempty"`
-	VolumeTemplateBootType             string          `json:"volume_template_boot_type,omitempty"`
-	VolumeTemplateDeprecationStatus    string          `json:"volume_template_deprecation_status,omitempty"`
-	VolumeTemplateRepoURL              string          `json:"volume_template_repo_url,omitempty"`
-	VolumeTemplateOperatingSystem      OperatingSystem `json:"volume_template_operating_system,omitempty"`
-	VolumeTemplateTags                 []string        `json:"volume_template_tags,omitempty"`
+	VolumeTemplateID                      int             `json:"volume_template_id,omitempty"`
+	VolumeTemplateLabel                   string          `json:"volume_template_label,omitempty"`
+	VolumeTemplateSizeMBytes              int             `json:"volume_template_size_mbytes,omitempty"`
+	VolumeTemplateDisplayName             string          `json:"volume_template_display_name,omitempty"`
+	VolumeTemplateDescription             string          `json:"volume_template_description,omitempty"`
+	VolumeTemplateLocalDiskSupported      bool            `json:"volume_template_local_supported,omitempty"`
+	VolumeTemplateBootMethodsSupported    string          `json:"volume_template_boot_methods_supported,omitempty"`
+	VolumeTemplateBootType                string          `json:"volume_template_boot_type,omitempty"`
+	VolumeTemplateDeprecationStatus       string          `json:"volume_template_deprecation_status,omitempty"`
+	VolumeTemplateRepoURL                 string          `json:"volume_template_repo_url,omitempty"`
+	VolumeTemplateOperatingSystem         OperatingSystem `json:"volume_template_operating_system,omitempty"`
+	VolumeTemplateTags                    []string        `json:"volume_template_tags,omitempty"`
+	VolumeTemplateOsBootstrapFunctionName string          `json:"volume_template_os_bootstrap_function_name,omitempty"`
 }
 
 //VolumeTemplates retrives the list of available templates
