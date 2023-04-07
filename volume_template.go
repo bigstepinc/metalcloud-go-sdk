b19a5fc810d73a38619c23354277c5bd95859a06
Aurelian Anghel <aurelian.anghel.ro@gmail.com>
2023-04-07 10:44:20 +0000

    added network operating system

--- volume_template.go
+++ volume_template.go
@@ -4,7 +4,7 @@ import "fmt"
 
 //go:generate go run helper/gen_exports.go
 
-//OperatingSystem describes an OS
+// OperatingSystem describes an OS
 type OperatingSystem struct {
 	OperatingSystemType         string `json:"operating_system_type,omitempty" yaml:"type,omitempty"`
 	OperatingSystemVersion      string `json:"operating_system_version,omitempty" yaml:"version,omitempty"`
@@ -12,15 +12,16 @@ type OperatingSystem struct {
 }
 
 type NetworkOperatingSystem struct {
-	OperatingSystemType            string `json:"operating_system_type,omitempty" yaml:"type,omitempty"`
-	OperatingSystemVersion         string `json:"operating_system_version,omitempty" yaml:"version,omitempty"`
-	OperatingSystemArchitecture    string `json:"operating_system_architecture,omitempty" yaml:"architecture,omitempty"`
-	OperatingSystemVendor          string `json:"operating_system_vendor,omitempty" yaml:"vendor,omitempty"`
-	OperatingSystemMachine         string `json:"operating_system_machine,omitempty" yaml:"machine,omitempty"`
-	OperatingSystemMachineRevision string `json:"operating_system_machine_revision,omitempty" yaml:"revisionMachine,omitempty"`
+	OperatingSystemArchitecture   string `json:"operating_system_architecture,omitempty" yaml:"architecture,omitempty"`
+	OperatingSystemDatacenterName string `json:"operating_system_datacenter_name,omitempty" yaml:"datacenter_name,omitempty"`
+	OperatingSystemMachine        string `json:"operating_system_machine,omitempty" yaml:"machine,omitempty"`
+	OperatingSystemSwitchDriver   string `json:"operating_system_switch_driver,omitempty" yaml:"switchDriver,omitempty"`
+	OperatingSystemSwitchRole     string `json:"operating_system_switch_role,omitempty" yaml:"switchRole,omitempty"`
+	OperatingSystemVendor         string `json:"operating_system_vendor,omitempty" yaml:"vendor,omitempty"`
+	OperatingSystemVersion        string `json:"operating_system_version,omitempty" yaml:"version,omitempty"`
 }
 
-//VolumeTemplate describes an OS template
+// VolumeTemplate describes an OS template
 type VolumeTemplate struct {
 	VolumeTemplateID                      int                    `json:"volume_template_id,omitempty"`
 	VolumeTemplateLabel                   string                 `json:"volume_template_label,omitempty"`
@@ -38,10 +39,11 @@ type VolumeTemplate struct {
 	VolumeTemplateNetworkOperatingSystem  NetworkOperatingSystem `json:"volume_template_network_operating_system,omitempty"`
 	VolumeTemplateVersion                 string                 `json:"volume_template_version,omitempty"`
 	VolumeTemplateIsExperimental          bool                   `json:"volume_template_is_experimental,omitempty"`
+	VolumeTemplateIsForSwitch             bool                   `json:"volume_template_is_for_switch,omitempty"`
 	VolumeTemplateOSReadyMethod           string                 `json:"volume_template_os_ready_method,omitempty"`
 }
 
-//VolumeTemplates retrives the list of available templates
+// VolumeTemplates retrives the list of available templates
 func (c *Client) VolumeTemplates() (*map[string]VolumeTemplate, error) {
 	var createdObject map[string]VolumeTemplate
 	userID := c.GetUserID()
@@ -74,7 +76,7 @@ func (c *Client) VolumeTemplates() (*map[string]VolumeTemplate, error) {
 	return &createdObject, nil
 }
 
-//volumeTemplateGet returns the specified volume template
+// volumeTemplateGet returns the specified volume template
 func (c *Client) volumeTemplateGet(volumeTemplateID id) (*VolumeTemplate, error) {
 	var createdObject VolumeTemplate
 
@@ -94,7 +96,7 @@ func (c *Client) volumeTemplateGet(volumeTemplateID id) (*VolumeTemplate, error)
 	return &createdObject, nil
 }
 
-//volumeTemplateCreate creates a private volume template from a drive
+// volumeTemplateCreate creates a private volume template from a drive
 func (c *Client) volumeTemplateCreateFromDrive(driveID id, objVolumeTemplate VolumeTemplate) (*VolumeTemplate, error) {
 	var createdObject VolumeTemplate
 
@@ -115,7 +117,7 @@ func (c *Client) volumeTemplateCreateFromDrive(driveID id, objVolumeTemplate Vol
 	return &createdObject, nil
 }
 
-//VolumeTemplateMakePublic makes a template public
+// VolumeTemplateMakePublic makes a template public
 func (c *Client) VolumeTemplateMakePublic(volumeTemplateID int, bootstrapFunctionName string) error {
 	resp, err := c.rpcClient.Call(
 		"volume_template_make_public",
@@ -134,7 +136,7 @@ func (c *Client) VolumeTemplateMakePublic(volumeTemplateID int, bootstrapFunctio
 	return nil
 }
 
-//VolumeTemplateMakePrivate makes a template private
+// VolumeTemplateMakePrivate makes a template private
 func (c *Client) VolumeTemplateMakePrivate(volumeTemplateID int, userID int) error {
 	resp, err := c.rpcClient.Call(
 		"volume_template_make_private",
