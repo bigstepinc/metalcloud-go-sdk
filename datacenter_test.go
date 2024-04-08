1acf3561a647964d0eb8d73c96984d050fe1a0e0
Yordan Galabov <galabovj@gmail.com>
2024-04-08 12:20:33 +0300

    fix test

--- datacenter_test.go
+++ datacenter_test.go
@@ -160,7 +160,7 @@ func TestDatacenterDeleteForApply(t *testing.T) {
 
 	RegisterTestingT(t)
 
-	responseBody = `{"result": [] ,"jsonrpc": "2.0","id": 0}`
+	responseBody = `{"result": {"DatacenterName": "dctest"}, "jsonrpc": "2.0", "id": 0}`
 
 	mc, err := GetMetalcloudClient("userEmail", "APIKey", httpServer.URL, false, "", "", "")
 	Expect(err).To(BeNil())
